b9d54805ce6a32554eccc07ab0ee9f035e4f3b61
Camilo3008 <camilo.alvarez3008@gmail.com>
2023-04-10 16:18:25 -0500

    xdddd

--- js/scrip.js
+++ js/scrip.js
@@ -1,1 +1,5 @@
-alert("especificaciones harware y software")
+function comprobar(){
+    let responsable
+    responsable=document.getElementById("responsable").value
+    document.getElementById("respuesta1").innerHTML="<h3> responsable:"+responsable+"</h3>"
+}
