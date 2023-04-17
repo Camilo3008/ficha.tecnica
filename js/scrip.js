dde6e2011cd7c5c8506918709828f69550973b3a
Camilo3008 <camilo.alvarez3008@gmail.com>
2023-04-17 18:35:51 -0500

    añadiendo documento javascrip

--- js/scrip.js
+++ js/scrip.js
@@ -1,5 +1,107 @@
 function comprobar(){
     let responsable
     responsable=document.getElementById("responsable").value
-    document.getElementById("respuesta1").innerHTML="<h3> responsable:"+responsable+"</h3>"
+    document.getElementById("respuesta1").innerHTML="<h3> Responsable:"+responsable+"</h3>"
+
+    let imagen
+    imagen=document.getElementById("imagen").value
+    document.getElementById("respuestaimagen").innerHTML="<h3> Logo: "+imagen+"</h3>"
+
+    let Nombreproduc
+    Nombreproduc=document.getElementById("NombreProducto").value
+    document.getElementById("respuesta2").innerHTML="<h3> Nombre del producto:"+Nombreproduc+"</h3>"
+
+    let lineaproduccion
+    lineaproduccion=document.getElementById("lineaproduc").value
+    document.getElementById("respuesta3").innerHTML="<h3> Linea de produccion:"+lineaproduccion+"</h3>"
+
+    let vercionesanteriores
+    vercionesanteriores=document.getElementById("Verc-anteriores").value
+    document.getElementById("respuesta4").innerHTML="<h3> Vercion anterior:"+vercionesanteriores+"</h3>"
+
+    let vercionactual
+    vercionactual=document.getElementById("verc-actual").value 
+    document.getElementById("respuesta5").innerHTML="<h3>Vercion actual:"+vercionactual+"</h3>"
+
+    let modulos
+    modulos=document.getElementById("modulos").value
+    document.getElementById("respuesta5").innerHTML="<h3>Modulos: "+modulos+"</h3>"
+
+    let descripciongen
+    descripciongen=document.getElementById("descripciongen").value
+    document.getElementById("respuesta6").innerHTML="<h3> Descripcion general del producto:"+descripciongen+"</h3>"
+
+    let objetivo
+    objetivo=document.getElementById("objetivo").value
+    document.getElementById("respuesta7").innerHTML="<h3> Objetivo:"+objetivo+"</h3>"
+
+    let descripcion
+    descripcion=document.getElementById("descripcion").value
+    document.getElementById("respuesta8").innerHTML="<h3> Descripcion:"+descripcion+"</h3>"
+
+    let harwServidor
+    harwServidor=document.getElementById("harwServidor").value
+    document.getElementById("respuesta9").innerHTML="<h3>Harware:"+harwServidor+"</h3>"
+
+    let softServidor
+    softServidor=document.getElementById("softServidor").value
+    document.getElementById("respuesta10").innerHTML="<h3> Software: "+softServidor+"</h3>"
+
+    let otrosServidor
+    otrosServidor=document.getElementById("otrosServidor").value
+    document.getElementById("respuesta11").innerHTML="<h3> Otros: "+otrosServidor+"</h3>"
+
+    let harwCliente
+    harwCliente=document.getElementById("harwCliente").value
+    document.getElementById("respuesta12").innerHTML="<h3>Harware:"+harwCliente+"</h3>"
+
+    let softCliente
+    softCliente=document.getElementById("softCliente").value
+    document.getElementById("respuesta13").innerHTML="<h3> Software:"+softCliente+"</h3>"
+
+    let otrosCliente
+    otrosCliente=document.getElementById("otrosCliente").value
+    document.getElementById("respuesta14").innerHTML="<h3> Otros:"+otrosCliente+"</h3>"
+
+    let reqFuncionales
+    reqFuncionales=document.getElementById("reqFuncionales").value
+    document.getElementById("respuesta15").innerHTML="<h3>Requerimientos funcionales:"+reqFuncionales+"</h3>"
+
+    let reqAdicionales
+    reqAdicionales=document.getElementById("reqAdicionales").value
+    document.getElementById("respuesta16").innerHTML="<h3>Requerimientos adicionales:"+reqAdicionales+"</h3>"
+
+    let clienProducto
+    clienProducto=document.getElementById("clienProducto").value
+    document.getElementById("respuesta17").innerHTML="<h3> Clientes del producto: "+clienProducto+"</h3>"
+
+    let vercion
+    vercion=document.getElementById("vercion").value
+    document.getElementById("respuesta18").innerHTML="<h3>Vercion:"+vercion+"</h3>"
+
+    let naturalezaCambio
+    naturalezaCambio=document.getElementById("naturalezaCambio").value
+    document.getElementById("respuesta19").innerHTML="<h3>Naturaleza de cambio:"+naturalezaCambio+"</h3>"
+
+    let fechAprovacion
+    fechAprovacion=document.getElementById("fechAprovacion").value
+    document.getElementById("respuesta20").innerHTML="<h3>Fecha de aprovacion: "+fechAprovacion+"</h3>"
+
+    let fechValidacion
+    fechValidacion=document.getElementById("fechValidacion").value
+    document.getElementById("respuesta21").innerHTML="<h3>Fecha de validacion: "+fechValidacion+"</h3>"
+
+    let elaboro
+    elaboro=document.getElementById("elaboro").value
+    document.getElementById("respuesta22").innerHTML="<h3>Elaboro: "+elaboro+"</h3>"
+
+    let aprobo
+    aprobo=document.getElementById("aprobo").value
+    document.getElementById("respuesta23").innerHTML="<h3>Aprobo:"+aprobo+"</h3>"
+
+    let valido
+    valido=document.getElementById("valido").value
+    document.getElementById("respuesta24").innerHTML="<h3>Valido: "+valido+"</h3>"
+
+
 }
